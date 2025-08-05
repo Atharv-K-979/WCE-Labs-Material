41b6318372d7d4a204cf48ee3cfc80fcd633677d
Atharv-pvt <atharvkulkarni4898@gmail.com>
2025-08-05 08:55:52 +0530

    Added Folders

---
+++ DS Lab/Assignment 1/1_SumOfGandS.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <list>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+#include <limits>
+#define ak long long
+#define kulkarni                 \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+using namespace std;
+/*
+Question:
+Time Complexity:
+Space Complexity:
+*/
+class Solution
+{
+public:
+    void solve()
+    {
+        int t = 5;
+        cout << t;
+    }
+};
+int main()
+{
+    kulkarni;
+    Solution atharv;
+    atharv.solve();
+    return 0;
+}
